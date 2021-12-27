4e52dee0cf9d295daf76c39c03245616078622b5
Testing4AI <95740042+Testing4AI@users.noreply.github.com>
2021-12-27 19:38:37 +0800

    Create prepare.py

---
+++ train_models/speech/prepare.py
@@ -0,0 +1,141 @@
+from tqdm import tqdm
+import requests
+import math
+import os
+import tarfile
+import numpy as np
+import librosa
+import argparse
+from keras.models import Sequential
+from kapre.time_frequency import Melspectrogram
+from kapre.utils import Normalization2D
+# clone from https://github.com/cleverhans-lab/entangled-watermark/blob/master/data/prepare_speechcmd.py
+
+
+def CustomPrepareGoogleSpeechCmd():
+    _DownloadGoogleSpeechCmdV2()
+    basePath = 'sd_GSCmdV2'
+
+    GSCmdV2Categs = {'left': 0, 'right': 1, 'up': 2, 'down': 3, 'yes': 4, 'no': 5, 'on': 6, 'off': 7, 'stop': 8,
+                     'go': 9}
+
+    WAV2Numpy(basePath + '/test/')
+    WAV2Numpy(basePath + '/train/')
+
+    trainWAVs = []
+    for root, dirs, files in os.walk(basePath + '/train/'):
+        if np.array([root.endswith(i) for i in GSCmdV2Categs.keys()]).any():
+            trainWAVs += [root + '/' + f for f in files if f.endswith('.wav.npy')]
+
+    testWAVsREAL = []
+    for root, dirs, files in os.walk(basePath + '/test/'):
+        if np.array([root.endswith(i) for i in GSCmdV2Categs.keys()]).any():
+            testWAVsREAL += [root + '/' + f for f in files if f.endswith('.wav.npy')]
+
+    trainWAVlabels = [_getFileCategory(f, GSCmdV2Categs) for f in trainWAVs]
+    testWAVREALlabels = [_getFileCategory(f, GSCmdV2Categs) for f in testWAVsREAL]
+
+    return np.array(trainWAVs), np.array(trainWAVlabels), np.array(testWAVsREAL), np.array(testWAVREALlabels)
+
+
+def _getFileCategory(file, catDict):
+    categ = os.path.basename(os.path.dirname(file))
+    return catDict.get(categ,0)
+
+
+def _DownloadGoogleSpeechCmdV2(forceDownload=False):
+    if os.path.isdir("sd_GSCmdV2/") and not forceDownload:
+        print('Google Speech commands dataset version 2 already exists. Skipping download.')
+    else:
+        if not os.path.exists("sd_GSCmdV2/"):
+            os.makedirs("sd_GSCmdV2/")
+        trainFiles = 'http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz'
+        testFiles = 'http://download.tensorflow.org/data/speech_commands_test_set_v0.02.tar.gz'
+        _downloadFile(testFiles, 'sd_GSCmdV2/test.tar.gz')
+        _downloadFile(trainFiles, 'sd_GSCmdV2/train.tar.gz')
+
+    if not os.path.isdir("sd_GSCmdV2/test/"):
+        _extractTar('sd_GSCmdV2/test.tar.gz', 'sd_GSCmdV2/test/')
+
+    if not os.path.isdir("sd_GSCmdV2/train/"):
+        _extractTar('sd_GSCmdV2/train.tar.gz', 'sd_GSCmdV2/train/')
+
+
+def _downloadFile(url, fName):
+    r = requests.get(url, stream=True)
+    total_size = int(r.headers.get('content-length', 0));
+    block_size = 1024
+    wrote = 0
+    print('Downloading {} into {}'.format(url, fName))
+    with open(fName, 'wb') as f:
+        for data in tqdm(r.iter_content(block_size), total=math.ceil(total_size // block_size), unit='KB',
+                         unit_scale=True):
+            wrote = wrote + len(data)
+            f.write(data)
+    if total_size != 0 and wrote != total_size:
+        print("ERROR, something went wrong")
+
+
+def _extractTar(fname, folder):
+    print('Extracting {} into {}'.format(fname, folder))
+    if (fname.endswith("tar.gz")):
+        tar = tarfile.open(fname, "r:gz")
+        tar.extractall(path=folder)
+        tar.close()
+    elif (fname.endswith("tar")):
+        tar = tarfile.open(fname, "r:")
+        tar.extractall(path=folder)
+        tar.close()
+
+
+def WAV2Numpy(folder, sr=None):
+    allFiles = []
+    for root, dirs, files in os.walk(folder):
+        allFiles += [os.path.join(root, f) for f in files if f.endswith('.wav')]
+
+    for file in tqdm(allFiles):
+        y, sr = librosa.load(file, sr=None)
+        np.save(file + '.npy', y)
+        os.remove(file)
+
+
+def load_audio(data, dim):
+    output = []
+    for i in data:
+        audio = np.load(i)
+        if audio.shape[0] == dim:
+            output.append(audio)
+        elif audio.shape[0] > dim:
+            randPos = np.random.randint(audio.shape[0] - dim)
+            output.append(audio[randPos:randPos + dim])
+        else:
+            randPos = np.random.randint(dim - audio.shape[0])
+            temp = np.zeros(dim)
+            temp[randPos:randPos + audio.shape[0]] = audio
+            output.append(temp)
+    return np.vstack(output)
+
+
+
+if __name__ == '__main__':
+    x_train, y_train, x_test, y_test = CustomPrepareGoogleSpeechCmd()
+    sr = 16000
+    iLen = 16000
+
+    melspecModel = Sequential()
+    melspecModel.add(Melspectrogram(n_dft=1024, n_hop=128, input_shape=(1, iLen),
+                                    padding='same', sr=sr, n_mels=80,
+                                    fmin=40.0, fmax=sr/2, power_melgram=1.0,
+                                    return_decibel_melgram=True, trainable_fb=False,
+                                    trainable_kernel=False,
+                                    name='mel_stft'))
+    melspecModel.add(Normalization2D(int_axis=0))
+    print("Converting audios to melspectrum")
+    x_train = melspecModel.predict(load_audio(x_train, iLen).reshape((-1, 1, iLen)))
+    x_test = melspecModel.predict(load_audio(x_test, iLen).reshape((-1, 1, iLen)))
+    np.save("sd_GSCmdV2/x_train.npy", x_train)
+    np.save("sd_GSCmdV2/y_train.npy", y_train)
+    np.save("sd_GSCmdV2/x_test.npy", x_test)
+    np.save("sd_GSCmdV2/y_test.npy", y_test)
+    print("Preprocessing finished")
+    
