530092d8f7d4f429f625a9eaba83669810c93326
Testing4AI <95740042+Testing4AI@users.noreply.github.com>
2023-03-09 19:38:31 +0800

    Update judge.py

--- DeepJudge/judge.py
+++ DeepJudge/judge.py
@@ -1,3 +1,5 @@
 # Thresholding & Judging
 
-# def calThreshold(contrast_models, metric_func, tests, confidence=0.99)
+# def calThreshold(contrast_models, metric_func, tests, confidence=0.99):
+
+# def Judge(suspect_model, values, thresholds, rule='major'):
